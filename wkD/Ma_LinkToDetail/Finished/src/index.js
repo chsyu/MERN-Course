dbc814082288c9160db054371e2c2f654ed548ea
chi-shan <chisamyu@seed.net.tw>
2016-07-03 18:03:12 +0800

    wkD-hashHistory

--- wkD/Ma_LinkToDetail/Finished/src/index.js
+++ wkD/Ma_LinkToDetail/Finished/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, browserHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import reducers from './reducers';
 import routes from './routes';
 import promise from 'redux-promise';
@@ -15,6 +15,6 @@ const store = createStoreWithMiddleware(reducers, window.devToolsExtension ? win
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory} routes={routes} />
+    <Router history={hashHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
