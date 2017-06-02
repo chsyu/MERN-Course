5af5cc7b080bfa26f64683a31982dddaebe1d350
Yu Chi-Shan <chsyu@tea.ntue.edu.tw>
2017-06-02 15:22:28 +0800

    wkC - browserHistory

--- wkD/M2_NestedRoute/Finished/src/index.js
+++ wkD/M2_NestedRoute/Finished/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, hashHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import reducers from './reducers';
 import routes from './routes';
 
@@ -10,6 +10,6 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={hashHistory} routes={routes} />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
